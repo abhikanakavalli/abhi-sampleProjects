916662ea77387d018c9576d670347f71b26ea847
abhikanakavalli <abhikanakavalli98@gmail.com>
2023-08-09 19:26:51 +0530

    [Commit check]- Checking push

--- src/AppLayout/Header.js
+++ src/AppLayout/Header.js
@@ -7,7 +7,7 @@ function Header() {
             <h1 class="text-white text-lg">About</h1>
             <div>
                 <button class='border-white border-2 rounded-sm bg-white mt-2'>
-                    SignOut
+                    sign Out
                 </button>
             </div>
             </div>
